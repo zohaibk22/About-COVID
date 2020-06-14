c2f9f72b13da5c3a218592d5d361ed153bd9b70d
Zohaib Khan <zohaibk1111@gmail.com>
2020-06-14 16:43:49 -0400

    creating synptoms and rememdies div/button

--- app.js
+++ app.js
@@ -79,15 +79,13 @@ const getCountryData = async (country) => {
 
 const buttonUs = document.querySelector(".us");
 buttonUs.addEventListener("click", () => {
-  buttonUs.style.backgroundImage = "none";
+  if (buttonUs.className === "country us hide-flag") {
+    buttonUs.className = "country us";
+  } else {
+    buttonUs.className = "country us hide-flag";
+  }
   getCountryData("us");
 });
-
-// const buttonBrazil = document.querySelector(".brazil");
-// buttonBrazil.addEventListener("click", () => {
-//   buttonBrazil.style.backgroundImage = "none";
-//   getCountryData("brazil");
-// });
 
 const buttonBrazil = document.querySelector(".brazil");
 buttonBrazil.addEventListener("click", () => {
@@ -101,90 +99,114 @@ buttonBrazil.addEventListener("click", () => {
 
 const buttonRussia = document.querySelector(".russia");
 buttonRussia.addEventListener("click", () => {
-  buttonRussia.style.backgroundImage = "none";
+  if (buttonRussia.className === "country russia hide-flag") {
+    buttonRussia.className = "country russia";
+  } else {
+    buttonRussia.className = "country russia hide-flag";
+  }
   getCountryData("russia");
 });
 
 const buttonIndia = document.querySelector(".india");
 buttonIndia.addEventListener("click", () => {
-  buttonIndia.style.backgroundImage = "none";
+  if (buttonIndia.className === "country india hide-flag") {
+    buttonIndia.className = "country india";
+  } else {
+    buttonIndia.className = "country india hide-flag";
+  }
   getCountryData("india");
 });
 
 const buttonUk = document.querySelector(".uk");
 buttonUk.addEventListener("click", () => {
-  buttonUk.style.backgroundImage = "none";
+  if (buttonUk.className === "country uk hide-flag") {
+    buttonUk.className = "country uk";
+  } else {
+    buttonUk.className = "country uk hide-flag";
+  }
   getCountryData("uk");
 });
 
 const buttonSpain = document.querySelector(".spain");
 buttonSpain.addEventListener("click", () => {
-  buttonSpain.style.backgroundImage = "none";
+  if (buttonSpain.className === "country spain hide-flag") {
+    buttonSpain.className = "country spain";
+  } else {
+    buttonSpain.className = "country spain hide-flag";
+  }
   getCountryData("spain");
 });
 
 const buttonItaly = document.querySelector(".italy");
 buttonItaly.addEventListener("click", () => {
-  buttonItaly.style.backgroundImage = "none";
+  if (buttonItaly.className === "country italy hide-flag") {
+    buttonItaly.className = "country italy";
+  } else {
+    buttonItaly.className = "country italy hide-flag";
+  }
   getCountryData("italy");
 });
 
 const buttonPeru = document.querySelector(".peru");
 buttonPeru.addEventListener("click", () => {
-  buttonPeru.style.backgroundImage = "none";
+  if (buttonPeru.className === "country peru hide-flag") {
+    buttonPeru.className = "country peru";
+  } else {
+    buttonPeru.className = "country peru hide-flag";
+  }
   getCountryData("peru");
 });
 
 const buttonGermany = document.querySelector(".germany");
 buttonGermany.addEventListener("click", () => {
-  buttonGermany.style.backgroundImage = "none";
+  if (buttonGermany.className === "country germany hide-flag") {
+    buttonGermany.className = "country germany";
+  } else {
+    buttonGermany.className = "country germany hide-flag";
+  }
   getCountryData("germany");
 });
 
 const buttonIran = document.querySelector(".iran");
 buttonIran.addEventListener("click", () => {
-  buttonIran.style.backgroundImage = "none";
+  if (buttonIran.className === "country iran hide-flag") {
+    buttonIran.className = "country iran";
+  } else {
+    buttonIran.className = "country iran hide-flag";
+  }
   getCountryData("iran");
 });
 
-function noOverLap() {
-  const buttonRemove = document.querySelector(".country");
-  buttonRemove.removeEventListener("click");
-}
+const getSymptoms = function () {
+  const symptomsArray = [
+    "Fever or Chills",
+    "Cough",
+    "Shortness of Breath",
+    "Fatigue",
+    "Muscle",
+    "Headache",
+    "Loss of Taste and/or Smell",
+    "Sore Throat",
+    "Congestion or Runny Nose",
+    "Nausea or Vomiting",
+    "Diarrhea",
+  ];
+  const parentDiv = document.querySelector(".general-information");
+  const symptomsDiv = document.createElement("div");
+  const symptomsUl = document.createElement("ul");
 
-//const mostRecentDate = new DataCue(Math.max.apply(null));
+  symptomsArray.forEach((element, i) => {
+    let symptomsListItems = (document.createElement("li").innerText =
+      symptomsArray[i]);
+    symptomsUl.append(symptomsListItems);
+  });
 
-// Some function to pull the most recent data by date --  not sure what exactly is going on
-// new Date(
-//   Math.max.apply(
-//     null,
-//     dataArray.map(function (e) {
-//       return new Date(e.MeasureDate);
-//     })
-//   )
-// );
+  symptomsDiv.append(symptomsUl);
+  parentDiv.append(symptomsDiv);
+};
+//getSymptoms();
 
-// console.log(Date);
-
-// const mostRecentDate = new Date(
-//   Math.max.apply(
-//     null,
-//     countryDataCovid((e) => {
-//       return new Date(e.MeasureData);
-//     })
-//   )
-// );
-
-// const mostRecentObject = countryDataCovid.filter((e) => {
-//   let d = new Date(e.MeasureDate);
-//   return d.getTime() == mostRecentDate.getTime();
-// })[0];
-
-// console.log(mostRecentObject());
-
-// console.log(countryDataCovid.Country);
-
-// console.log(response1);
-
-// let countryFlag = response2.data.name;
-// console.log(countryFlag);
+const symptomsButton = document.querySelector(".symptoms");
+symptomsButton.addEventListener("click", () => {
+  getSymptoms();
+});
